9505daddbc031a3d8c35903bc8fe40698272546b
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2023-09-18 19:14:40 +0200

    mypy: created a bunch of tests to check how it works
    - In particular, to test subtypes
    - Prototyped utils for testing with mypy, see mypytest_prototype.py
    - Commented out manny's tests

---
+++ tests/types/mypy_api_eg.py
@@ -0,0 +1,18 @@
+"""
+Simple example of how mypy.api works
+"""
+
+import sys
+from mypy import api
+
+result = api.run(sys.argv[1:])
+
+if result[0]:
+    print('\nType checking report:\n')
+    print(result[0])  # stdout
+
+if result[1]:
+    print('\nError report:\n')
+    print(result[1])  # stderr
+
+print('\nExit status:', result[2])
