1a1f1561c49444b4402a12d88b4ddc5f6d30ca79
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2023-09-18 23:35:13 +0200

    mypy: working test example  for abstractmethod and subtypes
    - See the last test at test_types.py, it uses a combination of bounds, Generic, and mypy --strict flag
    - Imports from the project had to be type hinted, so they pass mypy --strict checks
    - Deleted dangling pyproject.toml file

--- lib/bac.py
+++ lib/bac.py
@@ -3,11 +3,11 @@ import logging
 
 
 class BaseAbstractClass(ABC):
-    def __init__(self):
+    def __init__(self) -> None:
         self.logger = logging.getLogger(__name__)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{__name__}"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{__name__}"
